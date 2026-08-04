8b6586e8893f77742137726e7469a2230666f600
OsmauGon <mauricio7892@hotmail.com>
2026-08-04 20:46:37 -0300

    Vamos a levantar el backend en railway

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors'
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import authRoutes from './src/routes/auth.js'
 import "dotenv/config";
@@ -48,9 +47,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 */
 
-// Adaptador para Postgres
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-export const prisma = new PrismaClient({ adapter });
+
 
 app.use(express.json());
 
